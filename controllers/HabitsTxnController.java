ce8b615748b94fe5457b12e289d60934b8627c46
DileepGowd <dileepgowd12345@gmail.com>
2019-01-18 11:27:46 +0530

    Update HabitsTxnController.java

--- controllers/HabitsTxnController.java
+++ controllers/HabitsTxnController.java
@@ -22,7 +22,7 @@ import com.model.HabitsTxnDtl;
 import com.service.HabitService;
 import com.service.IHabitsTxnService;
 
-@CrossOrigin(origins ="*")
+@CrossOrigin(origins = "*")
 @Controller
 @RequestMapping("api/txn")
 public class HabitsTxnController {
@@ -31,26 +31,26 @@ public class HabitsTxnController {
 
 	@Autowired
 	private HabitService habitService;
-	
+
 	@GetMapping("habitTxn/{id}")
 	public ResponseEntity<HabitsTxn> getById(@PathVariable("id") String no) {
 		HabitsTxn test = habitTxnService.getByMrno(no);
 		return new ResponseEntity<HabitsTxn>(test, HttpStatus.OK);
 	}
-	@RequestMapping(value = "/habitTxn",consumes=MediaType.APPLICATION_JSON_UTF8_VALUE, method = RequestMethod.POST)
-	public ResponseEntity<Void> createHabitTxn(@RequestBody HabitsTxn habitTxn,UriComponentsBuilder builder) {
-		HabitsTxn txn=new HabitsTxn();
+
+	@RequestMapping(value = "/habitTxn", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, method = RequestMethod.POST)
+	public ResponseEntity<Void> createHabitTxn(@RequestBody HabitsTxn habitTxn, UriComponentsBuilder builder) {
+		HabitsTxn txn = new HabitsTxn();
 		System.out.println("Posting...!");
 		txn.setMediaclRecordNo(habitTxn.getMediaclRecordNo());
 		txn.setAdditionalNotes(habitTxn.getAdditionalNotes());
-		
+
 		HabitsTxnDtl txndtl;
-		
 
 		List<HabitsTxnDtl> list = new ArrayList<>();
 
 		for (int i = 0; i < habitTxn.getHabitsTxnDtls().size(); i++) {
-			
+
 			txndtl = new HabitsTxnDtl();
 			txndtl.setHabitsTxn(txn);
 			txndtl.setCreatedBy(habitTxn.getHabitsTxnDtls().get(i).getCreatedBy());
@@ -65,39 +65,44 @@ public class HabitsTxnController {
 		habitTxnService.createHabitTxn(txn);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
-	
-	@RequestMapping(value = "/habitTxnUpdate",consumes=MediaType.APPLICATION_JSON_UTF8_VALUE, method = RequestMethod.PUT)
-	public ResponseEntity<Void> updateHabitTxn(@RequestBody HabitsTxn habitTxn,UriComponentsBuilder builder) {
-		HabitsTxn txn=new HabitsTxn();
+
+	@RequestMapping(value = "/habitTxnUpdate", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, method = RequestMethod.PUT)
+	public ResponseEntity<Void> updateHabitTxn(@RequestBody HabitsTxn habitTxn, UriComponentsBuilder builder) {
+		HabitsTxn txn = new HabitsTxn();
 		System.out.println("updating...!");
 		txn.setId(habitTxn.getId());
 		txn.setMediaclRecordNo(habitTxn.getMediaclRecordNo());
 		txn.setAdditionalNotes(habitTxn.getAdditionalNotes());
-		
+
 		HabitsTxnDtl txndtl;
-		
 
 		List<HabitsTxnDtl> list = new ArrayList<>();
-			
-			txndtl = new HabitsTxnDtl();
-			txndtl.setHabitsTxn(txn);
-			txndtl.setId(habitTxn.getHabitsTxnDtls().get(0).getId());
-			txndtl.setCreatedBy(habitTxn.getHabitsTxnDtls().get(0).getCreatedBy());
-			txndtl.setCreatedOn(habitTxn.getHabitsTxnDtls().get(0).getCreatedOn());
-			txndtl.setDescription(habitTxn.getHabitsTxnDtls().get(0).getDescription());
-			txndtl.setHabit(habitTxn.getHabitsTxnDtls().get(0).getHabit());
-			txndtl.setUpdatedBy(habitTxn.getHabitsTxnDtls().get(0).getUpdatedBy());
-			txndtl.setUpdatedOn(habitTxn.getHabitsTxnDtls().get(0).getUpdatedOn());
-			list.add(txndtl);
+
+		txndtl = new HabitsTxnDtl();
+		txndtl.setHabitsTxn(txn);
+		txndtl.setId(habitTxn.getHabitsTxnDtls().get(0).getId());
+		txndtl.setCreatedBy(habitTxn.getHabitsTxnDtls().get(0).getCreatedBy());
+		txndtl.setCreatedOn(habitTxn.getHabitsTxnDtls().get(0).getCreatedOn());
+		txndtl.setDescription(habitTxn.getHabitsTxnDtls().get(0).getDescription());
+		txndtl.setHabit(habitTxn.getHabitsTxnDtls().get(0).getHabit());
+		txndtl.setUpdatedBy(habitTxn.getHabitsTxnDtls().get(0).getUpdatedBy());
+		txndtl.setUpdatedOn(habitTxn.getHabitsTxnDtls().get(0).getUpdatedOn());
+		list.add(txndtl);
 		txn.setHabitsTxnDtls(list);
 		habitTxnService.updateHabitTxn(txn);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
-	
+
 	@GetMapping("habitsList")
 	public ResponseEntity<List<Habit>> getAllHabits() {
 		List<Habit> habit = habitService.getAllHabits();
 		return new ResponseEntity<List<Habit>>(habit, HttpStatus.OK);
 	}
-	
+
+	@GetMapping("habits/{id}")
+	public ResponseEntity<Habit> getByHabitId(@PathVariable("id") int no) {
+		Habit habit = habitService.getByHabit(no);
+		return new ResponseEntity<Habit>(habit, HttpStatus.OK);
+	}
+
 }
